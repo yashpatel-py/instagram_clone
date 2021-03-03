24b4771a86a086e62c82c060a9ecfd72807dc753
yash-2115 <yashp63544@gmail.com>
2021-03-03 23:37:23 +0530

    added follow back button in notifaction page

--- notifications/views.py
+++ notifications/views.py
@@ -5,30 +5,31 @@ from django.http import HttpResponse
 from notifications.models import Notification
 # Create your views here.
 
+
 def ShowNOtifications(request):
-	user = request.user
-	notifications = Notification.objects.filter(user=user).order_by('-date')
-	Notification.objects.filter(user=user, is_seen=False).update(is_seen=True)
+    user = request.user
+    notifications = Notification.objects.filter(user=user).order_by('-date')
+    Notification.objects.filter(user=user, is_seen=False).update(is_seen=True)
 
-	template = loader.get_template('notifications.html')
+    template = loader.get_template('notifications.html')
 
-	context = {
-		'notifications': notifications,
-	}
+    context = {
+        'notifications': notifications,
+    }
 
-	return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(context, request))
+
 
 def DeleteNotification(request, noti_id):
-	user = request.user
-	Notification.objects.filter(id=noti_id, user=user).delete()
-	return redirect('show-notifications')
+    user = request.user
+    Notification.objects.filter(id=noti_id, user=user).delete()
+    return redirect('show-notifications')
 
 
 def CountNotifications(request):
-	count_notifications = 0
-	if request.user.is_authenticated:
-		count_notifications = Notification.objects.filter(user=request.user, is_seen=False).count()
+    count_notifications = 0
+    if request.user.is_authenticated:
+        count_notifications = Notification.objects.filter(
+            user=request.user, is_seen=False).count()
 
-	return {'count_notifications':count_notifications}
-	
-
+    return {'count_notifications': count_notifications}
