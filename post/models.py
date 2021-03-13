3f28855835c8fc23b27cb97890ad9f48ff2ca0be
yash-2115 <yashp63544@gmail.com>
2021-03-13 23:28:04 +0530

    bug fixsss

--- post/models.py
+++ post/models.py
@@ -18,105 +18,119 @@ def user_directory_path(instance, filename):
 
 
 class Tag(models.Model):
-	title = models.CharField(max_length=75, verbose_name='Tag')
-	slug = models.SlugField(null=False, unique=True)
+    title = models.CharField(max_length=75, verbose_name='Tag')
+    slug = models.SlugField(null=False, unique=True)
 
-	class Meta:
-		verbose_name='Tag'
-		verbose_name_plural = 'Tags'
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
 
-	def get_absolute_url(self):
-		return reverse('tags', args=[self.slug])
-		
-	def __str__(self):
-		return self.title
+    def get_absolute_url(self):
+        return reverse('tags', args=[self.slug])
 
-	def save(self, *args, **kwargs):
-		if not self.slug:
-			self.slug = slugify(self.title)
-		return super().save(*args, **kwargs)
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
 
 class PostFileContent(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='content_owner')
-	file = models.FileField(upload_to=user_directory_path)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='content_owner')
+    file = models.FileField(upload_to=user_directory_path)
+
 
 class Post(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	content =  models.ManyToManyField(PostFileContent, related_name='contents')
-	caption = models.TextField(max_length=1500, verbose_name='Caption')
-	posted = models.DateTimeField(auto_now_add=True)
-	tags = models.ManyToManyField(Tag, related_name='tags')
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	likes = models.IntegerField(default=0)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.ManyToManyField(PostFileContent, related_name='contents')
+    caption = models.TextField(max_length=1500, verbose_name='Caption')
+    posted = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='tags')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
 
+    def get_absolute_url(self):
+        return reverse('postdetails', args=[str(self.id)])
 
-	def get_absolute_url(self):
-		return reverse('postdetails', args=[str(self.id)])
-
-	def __str__(self):
-		return str(self.id)
+    def __str__(self):
+        return str(self.id)
 
 
 class Follow(models.Model):
-	follower = models.ForeignKey(User,on_delete=models.CASCADE, null=True, related_name='follower')
-	following = models.ForeignKey(User,on_delete=models.CASCADE, null=True, related_name='following')
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name='follower')
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name='following')
 
-	def user_follow(sender, instance, *args, **kwargs):
-		follow = instance
-		sender = follow.follower
-		following = follow.following
-		notify = Notification(sender=sender, user=following, notification_type=3)
-		notify.save()
+    def user_follow(sender, instance, *args, **kwargs):
+        follow = instance
+        sender = follow.follower
+        following = follow.following
+        notify = Notification(
+            sender=sender, user=following, notification_type=3)
+        notify.save()
 
-	def user_unfollow(sender, instance, *args, **kwargs):
-		follow = instance
-		sender = follow.follower
-		following = follow.following
+    def user_unfollow(sender, instance, *args, **kwargs):
+        follow = instance
+        sender = follow.follower
+        following = follow.following
 
-		notify = Notification.objects.filter(sender=sender, user=following, notification_type=3)
-		notify.delete()
+        notify = Notification.objects.filter(
+            sender=sender, user=following, notification_type=3)
+        notify.delete()
+
 
 class Stream(models.Model):
-    following = models.ForeignKey(User, on_delete=models.CASCADE,null=True, related_name='stream_following')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)   
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name='stream_following')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField()
 
     def add_post(sender, instance, *args, **kwargs):
-    	post = instance
-    	user = post.user
-    	followers = Follow.objects.all().filter(following=user)
-    	for follower in followers:
-    		stream = Stream(post=post, user=follower.follower, date=post.posted, following=user)
-    		stream.save()
+        post = instance
+        user = post.user
+        followers = Follow.objects.all().filter(following=user)
+        for follower in followers:
+            stream = Stream(post=post, user=follower.follower,
+                            date=post.posted, following=user)
+            stream.save()
+
 
 class Likes(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_like')
-	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_like')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_like')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='post_like')
 
-	def user_liked_post(sender, instance, *args, **kwargs):
-		like = instance
-		post = like.post
-		sender = like.user
-		notify = Notification(post=post, sender=sender, user=post.user, notification_type=1)
-		notify.save()
+    def user_liked_post(sender, instance, *args, **kwargs):
+        like = instance
+        post = like.post
+        sender = like.user
+        notify = Notification(post=post, sender=sender,
+                              user=post.user, notification_type=1)
+        notify.save()
 
-	def user_unlike_post(sender, instance, *args, **kwargs):
-		like = instance
-		post = like.post
-		sender = like.user
+    def user_unlike_post(sender, instance, *args, **kwargs):
+        like = instance
+        post = like.post
+        sender = like.user
 
-		notify = Notification.objects.filter(post=post, sender=sender, notification_type=1)
-		notify.delete()
+        notify = Notification.objects.filter(
+            post=post, sender=sender, notification_type=1)
+        notify.delete()
 
 
-#Stream
+# Stream
 post_save.connect(Stream.add_post, sender=Post)
 
-#Likes
+# Likes
 post_save.connect(Likes.user_liked_post, sender=Likes)
 post_delete.connect(Likes.user_unlike_post, sender=Likes)
 
-#Follow
+# Follow
 post_save.connect(Follow.user_follow, sender=Follow)
 post_delete.connect(Follow.user_unfollow, sender=Follow)
